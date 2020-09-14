ef476b7e51f63bfa62121ab5eaac6b6afc202aa4
mtgimenez.prof <mtgimenez.prof@gmail.com>
2020-09-14 10:27:29 +0200

    dddd

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ html_theme = 'alabaster'
 html_theme_options = {
    'body_text_align': 'justify',
    'font_family': 'Roboto',
-   'body_max_width': '90%',
+   'body_max_width': '1024px',
    'page_width':'70%',
    'sidebar_width':'30%',
 }
