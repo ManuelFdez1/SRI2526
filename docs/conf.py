410494a41ca8b2fca245cf0654e8b478cdfe15b2
mtgimenez.prof <mtgimenez.prof@gmail.com>
2023-02-07 19:51:56 +0100

    ccc

--- docs/conf.py
+++ docs/conf.py
@@ -75,8 +75,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'haiku'
-#html_theme = "sphinx_rtd_theme"
+#html_theme = 'haiku'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -97,8 +97,7 @@ html_title='ASIR - Servicios de Red e Internet'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-#html_style = "css/my_theme.css"
-html_css_files = ['haiku.css','css/my_theme.css']
+html_style = "css/my_theme.css"
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
